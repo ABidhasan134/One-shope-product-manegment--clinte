b6c7f70dc32fd31015308af12862b931b583d98d
ABid hasan <abeydhasan134@gmail.com>
2024-08-15 02:07:16 +0600

    layout

---
+++ src/layout/main.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import NavBar from '../global/navBar'
+import Footer from '../global/footer'
+import { Outlet } from 'react-router-dom'
+
+const Main = () => {
+  return (
+    <div className='border-2 border-red-600 container mx-auto p-0 grid justify-center'>
+      <NavBar></NavBar>
+      <div>
+        <Outlet></Outlet>
+      </div>
+      <Footer></Footer>
+    </div>
+  )
+}
+
+export default Main
